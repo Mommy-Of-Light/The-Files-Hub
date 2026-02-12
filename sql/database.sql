af8fe0cb0bfdeeaaa03ebd0c90f649498f85c59b
bastien-jcln <bastien.jcln@eduge.ch>
2026-02-12 09:03:39 +0100

    légere modif de la base de donnée

--- sql/database.sql
+++ sql/database.sql
@@ -1,21 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1deb3
--- https://www.phpmyadmin.net/
---
--- Hôte : localhost:3306
--- Généré le : jeu. 22 jan. 2026 à 07:36
--- Version du serveur : 10.11.13-MariaDB-0ubuntu0.24.04.1
--- Version de PHP : 8.4.17
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de données : `file_hub`
@@ -34,8 +19,8 @@ USE file_hub;
 CREATE TABLE `Posts` (
   `idPost` int(11) UNSIGNED NOT NULL,
   `idCreator` int(11) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
   `fileLink` text NOT NULL,
-  `createDate` date NOT NULL,
   `description` longtext NOT NULL,
   `likes` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -48,7 +33,8 @@ CREATE TABLE `Posts` (
 
 CREATE TABLE `PostsUsers` (
   `idPosts` int(10) UNSIGNED NOT NULL,
-  `idUser` int(10) UNSIGNED NOT NULL
+  `idUser` int(10) UNSIGNED NOT NULL,
+  `CreatedAt` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -64,9 +50,18 @@ CREATE TABLE `Users` (
   `userName` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(256) NOT NULL,
-  `level` int(11) NULL DEFAULT 0,
+  `roles` int(11) DEFAULT 0 COMMENT '0: user\r\n1: admin\r\n2: opérateur\r\n 3: creator',
+  `level` int(11) DEFAULT 0,
+  `xp` int(11) DEFAULT 0,
   `profilePicture` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `Users`
+--
+
+INSERT INTO `Users` (`idUser`, `firstName`, `lastName`, `userName`, `email`, `password`, `roles`, `level`, `xp`, `profilePicture`) VALUES
+(1, 'Dev', 'Admin', 'Creator', 'empress.mommy.of.light@gmail.com', '$2y$12$DLfRhyHaj0cfeNgfAmXKb.NX3FgTO3b/rhsSEEI.b8CJntg2QbEZa', 3, 0, 0, 'Creator_pfp.png');
 
 --
 -- Index pour les tables déchargées
@@ -92,7 +87,7 @@ ALTER TABLE `PostsUsers`
 ALTER TABLE `Users`
   ADD PRIMARY KEY (`idUser`),
   ADD UNIQUE KEY `email` (`email`),
-  ADD UNIQUE KEY `username` (`username`);
+  ADD UNIQUE KEY `username` (`userName`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
@@ -108,7 +103,7 @@ ALTER TABLE `Posts`
 -- AUTO_INCREMENT pour la table `Users`
 --
 ALTER TABLE `Users`
-  MODIFY `idUser` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `idUser` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Contraintes pour les tables déchargées
@@ -127,10 +122,3 @@ ALTER TABLE `PostsUsers`
   ADD CONSTRAINT `PostsUsers_ibfk_1` FOREIGN KEY (`idPosts`) REFERENCES `Posts` (`idPost`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `PostsUsers_ibfk_2` FOREIGN KEY (`idUser`) REFERENCES `Users` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
-
-ALTER TABLE `Users` ADD `roles` INT(11) NULL DEFAULT '0' COMMENT '0: user\r\n1: admin\r\n2: opérateur\r\n 3: creator' AFTER `password`;
-ALTER TABLE `Users` ADD `xp` INT(11) NULL DEFAULT '0' AFTER `level`;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
